7e34e42cd565f84c6bd24aa888fc5e81324e175b
John Burdick <john.burdick@gmail.com>
2025-08-22 18:07:07 -0500

    added switch Raw

--- cmdlet/Get-LifeNumber.ps1
+++ cmdlet/Get-LifeNumber.ps1
@@ -1,21 +1,22 @@
 function Get-LifeNumber {
     param (
     [Parameter(Mandatory = $true)]   
-    [System.DateOnly]$BirthDate 
+    [System.DateOnly]$BirthDate,
+    [switch]$Raw 
     )
    $table = (
-    @{number = 1; Type ='The Leader'; values = 'Leadership, independence'},
-    @{number = 2; Type ='The Peacemaker'; values = 'Compassion, determination'},
-    @{number = 3; Type ='The Creative'; values = 'Magnetism, extroversion, communication skills'},
-    @{number = 4; Type ='The Manager'; values = ' Stability, logic, loyalty'},
-    @{number = 5; Type ='The Adventurer'; values = ' Free-spirited, adaptability'},
-    @{number = 6; Type ='The Nurturer'; values = 'Protective, selflessness'},
-    @{number = 7; Type ='The Seeker'; values = 'Curiosity, thoroughness'},
-    @{number = 8; Type ='The Powerhouse'; values = 'Realism, unity'},
-    @{number = 9; Type ='The Humanitarian'; values = 'Integrity, acceptance'},
-    @{number = 11; Type ='The Inspired Healer'; values = 'Spirituality, balance, positivity'},
-    @{number = 22; Type ='The Master Teacher'; values = 'Organization, creativity, practicality'},
-    @{number = 33; Type ='The Illuminated Guide'; values = 'Compassion, creativity, responsibility'}
+        @{number = 1; Type ='The Leader'; values = 'Leadership, independence'},
+        @{number = 2; Type ='The Peacemaker'; values = 'Compassion, determination'},
+        @{number = 3; Type ='The Creative'; values = 'Magnetism, extroversion, communication skills'},
+        @{number = 4; Type ='The Manager'; values = ' Stability, logic, loyalty'},
+        @{number = 5; Type ='The Adventurer'; values = 'Free-spirited, adaptability'},
+        @{number = 6; Type ='The Nurturer'; values = 'Protective, selflessness'},
+        @{number = 7; Type ='The Seeker'; values = 'Curiosity, thoroughness'},
+        @{number = 8; Type ='The Powerhouse'; values = 'Realism, unity'},
+        @{number = 9; Type ='The Humanitarian'; values = 'Integrity, acceptance'},
+        @{number = 11; Type ='The Inspired Healer'; values = 'Spirituality, balance, positivity'},
+        @{number = 22; Type ='The Master Teacher'; values = 'Organization, creativity, practicality'},
+        @{number = 33; Type ='The Illuminated Guide'; values = 'Compassion, creativity, responsibility'}
    )
 
     function reduce {
@@ -36,8 +37,8 @@ function Get-LifeNumber {
     }
     $lifenumber = reduce $parts
     $result = $table | Where-Object { $_.number -eq $lifenumber }
-
-    return "Your Life Number is $lifenumber, which represents '$($result.Type)'. The key values associated with this number are: $($result.values)."
+    if ($Raw.IsPresent){ $result }
+    else{ return "Your Life Number is $lifenumber, which represents '$($result.Type)'. The key values associated with this number are: $($result.values)." }
 }
 
 
